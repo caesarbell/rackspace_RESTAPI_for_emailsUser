df1871841f448f526bba3d9aff7a7ba814aeb32d
Caesar Bell <caesar.jd.bell@gmail.com>
2015-11-25 15:03:41 -0500

    This is used to get information on a single user within your domain hosted on Rackspace

---
+++ getUserInfo.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * This is used for getting a single user in your domain information. 
+ *
+ * Contributors:
+ *
+ * - Rackspace Email & Apps REST API Team
+ * - Josh Shilling
+ */
+header('Content-Type:  text/plain');
+require_once 'ApiClient.php';
+
+$client = new  ApiClient();
+
+$response = $client->get(
+    '/customers/346345/domains/company_domain.com/ex/mailboxes/username_you_are_targetting/permissions',
+    'text/xml');
+
+echo  $response . "\r\n";
+?>
