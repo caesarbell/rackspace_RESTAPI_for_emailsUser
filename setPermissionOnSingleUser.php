d916d92f8399d4827a20d8d80bf6f79624d37be2
Caesar Bell <caesar.jd.bell@gmail.com>
2015-11-25 15:05:17 -0500

    Sets permission on a single user within your domain

---
+++ setPermissionOnSingleUser.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * This is used for setting a permission for a single user in your domain information.
+ *
+ * Contributors:
+ *
+ * - Rackspace Email & Apps REST API Team
+ * - Josh Shilling
+ */
+header('Content-Type:  text/plain');
+require_once 'ApiClient.php';
+
+$client = new  ApiClient();
+
+$fields = Array('permission' => 'both'); 
+
+$response = $client->post(
+    '/customers/346345/domains/company_domain.com/ex/mailboxes/username_you_are_targetting/permissions/admin.user',
+    $fields,
+    'txt/xml');
+
+echo  $response . "\r\n";
+?>
