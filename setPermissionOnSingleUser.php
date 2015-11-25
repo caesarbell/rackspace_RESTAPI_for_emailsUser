b23f68b42a8bcbf78e61b3c41ed1d41adb52e130
Caesar Bell <caesar.jd.bell@gmail.com>
2015-11-25 16:00:43 -0500

    Sets permission on single users, mod so there is no exisiting user data in file

--- setPermissionOnSingleUser.php
+++ setPermissionOnSingleUser.php
@@ -15,7 +15,7 @@ $client = new  ApiClient();
 $fields = Array('permission' => 'both'); 
 
 $response = $client->post(
-    '/customers/346345/domains/company_domain.com/ex/mailboxes/username_you_are_targetting/permissions/admin.user',
+    '/customers/customer_number/domains/company_domain.com/ex/mailboxes/username_you_are_targetting/permissions/admin.user',
     $fields,
     'txt/xml');
 
